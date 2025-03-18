b67040f4b39e9f75a2be77677040e6c9b14423ed
ncaralov <ncaralov@axarea.com>
2025-03-18 19:57:25 +0300

    upd: strtools

--- internal/pkg/string-tools/case.go
+++ internal/pkg/string-tools/case.go
@@ -2,25 +2,39 @@ package strtools
 
 import "strings"
 
-// SnakeCase a helper function to convert camelCase to snake_case
+// SnakeCase converts camelCase or PascalCase to snake_case
 func SnakeCase(str string) string {
 	var result []rune
+	prevLower := false
+
 	for i, c := range str {
-		if i > 0 && 'A' <= c && c <= 'Z' {
-			result = append(result, '_')
+		if i > 0 && ('A' <= c && c <= 'Z') {
+			// Only insert underscore if the previous character is lowercase or next is lowercase (handling InvoiceP2P case)
+			if prevLower || (i+1 < len(str) && 'a' <= rune(str[i+1]) && rune(str[i+1]) <= 'z') {
+				result = append(result, '_')
+			}
 		}
 		result = append(result, rune(strings.ToLower(string(c))[0]))
+		prevLower = ('a' <= c && c <= 'z')
 	}
+
 	return string(result)
 }
 
 func KebabCase(str string) string {
 	var result []rune
+	prevLower := false
+
 	for i, c := range str {
-		if i > 0 && 'A' <= c && c <= 'Z' {
-			result = append(result, '-')
+		if i > 0 && ('A' <= c && c <= 'Z') {
+			// Only insert underscore if the previous character is lowercase or next is lowercase (handling InvoiceP2P case)
+			if prevLower || (i+1 < len(str) && 'a' <= rune(str[i+1]) && rune(str[i+1]) <= 'z') {
+				result = append(result, '-')
+			}
 		}
 		result = append(result, rune(strings.ToLower(string(c))[0]))
+		prevLower = ('a' <= c && c <= 'z')
 	}
+
 	return string(result)
 }
